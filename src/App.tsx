fa2a47f4dd9f43662a68a5cbe76661c70b457a9d
Javier Barrios <jbarrios90@live.com>
2024-12-19 22:25:58 -0300

    added some minor features and fix issue with tailwind css config

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,18 @@ function App() {
 
   return (
     <>
-      <h1>Contador de Calorias</h1>
+      <header className="bg-lime-600 py-3">
+        <div className="max-w-4xl mx-auto flex justify-between">
+          <h1 className="text-center text-lg font-bold text-white uppercase">
+            Contador de Calorias
+          </h1>
+        </div>
+        <section className="bg-lime-500 py-20 px-5">
+          <div className="max-w-4xl mx-auto">
+            <p>Formulario Aqui</p>
+          </div>
+        </section>
+      </header>
     </>
   )
 }
