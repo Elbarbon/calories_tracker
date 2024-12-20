424d6d8387c16f1335bdfc1c7b599c7ed3aeca4a
Javier Barrios Almendares <el.barbon@Maquina-virtual-de-Javier.local>
2024-12-20 11:08:24 -0300

    little update

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import { categories } from "../data/category"
 
 export default function Form() {
   return (
-    <div>Form</div>
+    <form
+      className="space-y-5 bg-white p-10 rounder-lg"
+    >
+      <div className="grid grid-cols-1 gap-3">
+        <label htmlFor="">Categoria</label>
+        <select
+          className="border border-slate-300 p-2 rounder-lg w-full bg-white" id="category"
+        >
+          
+        </select>
+      </div>
+    </form>
   )
 }
